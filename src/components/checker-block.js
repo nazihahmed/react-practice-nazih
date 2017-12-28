ed8d0c1b876ae9f76929138a0c68279eeb2ecd6d
maxpcs <max.pcs111@gmail.com>
2017-12-28 18:35:59 +0200

    basic player movement

--- src/components/checker-block.js
+++ src/components/checker-block.js
@@ -10,7 +10,9 @@ const CheckerBlock = (props) => {
     5: [2,4,6],
     6: [1,3,5,7]
   }
+
   let {x,y} = props;
+
   function checkPlayer() {
     if (x < 3) {
       if(player1[x].indexOf(y)!==-1) {
@@ -28,8 +30,15 @@ const CheckerBlock = (props) => {
       return 0;
     }
   }
+
+  function checkIndicator() {
+    return (x == props.indicator[0] && y == props.indicator[1]);
+  }
   return (
-    <td className={'checker-block '+(props.dark ?'checker-block--dark':'') + ' checker-x'+x+' checker-y'+y + (checkPlayer()==1?' checker-block__player1':'') + (checkPlayer()==2?' checker-block__player2':'')}></td>
+    <td className={'checker-block '+(props.dark ?'checker-block--dark':'') +
+      ' checker-x'+x+' checker-y'+y + (checkPlayer()==1?' checker-block__player1':'') +
+      (checkPlayer()==2?' checker-block__player2':'') + (checkIndicator()?' checker-block__active':'')}>
+    </td>
   );
 };
 
--- src/components/checkers-game.js
+++ src/components/checkers-game.js
@@ -9,14 +9,53 @@ export default class CheckersGame extends Component {
       y:7
     }
     this.state = {
-      isReset: false
+      isReset: false,
+      indicator: [3,4],
+      locked: false
     }
   }
+  handleKeyDown(e) {
+    let keys = {
+      37:'left',
+      39:'right',
+      38:'up',
+      40:'down',
+      13:'enter'
+    }
 
-  // initializePlayers() {
-  //
-  // }
-
+    let indicator = this.state.indicator;
+    let {x,y} = this.scale;
+    switch(keys[e.keyCode]){
+      case 'left':
+        if (indicator[1]>1) {
+          this.setState({'indicator':[indicator[0],indicator[1]-1]});
+        }
+        break;
+      case 'right':
+        if (indicator[1]<=x) {
+          this.setState({'indicator':[indicator[0],indicator[1]+1]});
+        }
+        break;
+      case 'up':
+        if (indicator[0]>1) {
+          this.setState({'indicator':[indicator[0]-1,indicator[1]]});
+        }
+        break;
+      case 'down':
+        if (indicator[0]<y) {
+          this.setState({'indicator':[indicator[0]+1,indicator[1]]});
+        }
+        break;
+      case 'enter':
+        console.log('lock this stone to move it');
+        this.setState({'locked':!this.state.locked});
+      default:
+        console.log('else',e.keyCode);
+    }
+  }
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this), false);
+  }
   renderGame() {
     let game = [];
     let flip = false;
@@ -25,13 +64,12 @@ export default class CheckersGame extends Component {
     for (let i = 0;i<x;i++){
       for(let b = 0;b<y;b++) {
         if(flip) {
-          game.push(<CheckerBlock key={'checker'+b+i} x={i+1} y={b+1} dark/>);
+          game.push(<CheckerBlock key={'checker'+b+i} x={i+1} y={b+1} dark indicator={this.state.indicator}/>);
         } else {
-          game.push(<CheckerBlock key={'checker'+b+i} x={i+1} y={b+1}/>);
+          game.push(<CheckerBlock key={'checker'+b+i} x={i+1} y={b+1} indicator={this.state.indicator}/>);
         }
         flip = !flip;
       }
-      // flip = true;
     }
     return game;
   }
